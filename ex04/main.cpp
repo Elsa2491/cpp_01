67fe2d7f927933b8d037e2e707e94f4fa98bd293
elsa2491 <elsa.touma@gmail.com>
2024-08-21 16:20:09 +0200

    [21.08 Done] Protection si argc != 4

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: eltouma <eltouma@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/21 12:42:33 by eltouma           #+#    #+#             */
-/*   Updated: 2024/08/21 16:06:06 by eltouma          ###   ########.fr       */
+/*   Updated: 2024/08/21 16:19:34 by eltouma          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,17 @@
 
 int main(int argc, char **argv)
 {
-	(void)argc;
-	(void)argv;
 	std::string read;
 	std::string file_name(argv[1]); 
 	std::string extension(".replace");
 	std::string outfile_name; 
 	std::size_t	pos;
 
+	if (argc != 4)
+	{
+		std::cout << "Wrong number of arguments" << std::endl;
+		return (1);
+	}
 	outfile_name = file_name + extension;
 	std::ifstream file(argv[1], std::ios::in); // peut-etre que le mode n'est pas obligatoire
 	std::ofstream replace(outfile_name.c_str(), std::ios::out);
