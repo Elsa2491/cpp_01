2627fe3d5871c68a0945753e102d8f72d76bc5fc
elsa2491 <elsa.touma@gmail.com>
2024-08-21 16:18:15 +0200

    [21.08 Done] Fait dans le main. Le refaire avec fonction

---
+++ ex04/main.cpp
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eltouma <eltouma@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/21 12:42:33 by eltouma           #+#    #+#             */
+/*   Updated: 2024/08/21 16:06:06 by eltouma          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <cstring>
+
+int main(int argc, char **argv)
+{
+	(void)argc;
+	(void)argv;
+	std::string read;
+	std::string file_name(argv[1]); 
+	std::string extension(".replace");
+	std::string outfile_name; 
+	std::size_t	pos;
+
+	outfile_name = file_name + extension;
+	std::ifstream file(argv[1], std::ios::in); // peut-etre que le mode n'est pas obligatoire
+	std::ofstream replace(outfile_name.c_str(), std::ios::out);
+	if (!file.is_open())
+	{
+		std::cout << "Failed to open " << file << std::endl;
+		return (1);
+	}
+	if (!replace.is_open())
+	{
+		std::cout << "Failed to open " << replace << std::endl;
+		return (1);
+	}
+	while (std::getline(file, read))
+	{
+		pos = 0;
+		
+		while ((pos = read.find(argv[2], pos)) != std::string::npos)
+		{
+			replace << read.substr(0, pos);
+			replace << argv[3];
+			pos += strlen(argv[2]);
+			read = read.substr(pos);
+			pos = 0;
+		}
+		replace << read;
+		replace << std::endl;
+	}
+	return (0);
+}
