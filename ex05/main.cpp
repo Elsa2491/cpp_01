7abde639cbb858775058773e917691cd4dfd61e3
elsa2491 <elsa.touma@gmail.com>
2024-08-23 16:48:32 +0200

    [23.08 Change main] Update main with while loop and std::getline()

--- ex05/main.cpp
+++ ex05/main.cpp
@@ -6,17 +6,39 @@
 /*   By: eltouma <eltouma@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/21 17:37:53 by eltouma           #+#    #+#             */
-/*   Updated: 2024/08/21 19:35:46 by eltouma          ###   ########.fr       */
+/*   Updated: 2024/08/23 16:46:59 by eltouma          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Harl.hpp"
 
+/*
 int	main(void)
 {
 	Harl harl;
 	std::string level;
-	std::cout << "Enter a level: " << std::endl;
+	std::cout << "Welcome and nice to meet you Harl!\n"
+	<< "Please complain: BEBUG, INFO, WARNING, ERROR" << std::endl;
+	while (std::getline(std::cin, level))
+	{
+		if (!level.compare("WARNING") || !level.compare("DEBUG") || !level.compare("INFO") || !level.compare("ERROR"))
+		{
+			harl.complain(level);
+			return (0);
+		}
+		else
+			std::cout << "I don't understand. Please try again" << std::endl;
+	}
+}
+*/
+
+int main(void)
+{
+	Harl harl;
+	std::string level;
+
+	std::cout << "Welcome and nice to meet you Harl!\n"
+	<< "Please complain: BEBUG, INFO, WARNING, ERROR" << std::endl;
 	std::cin >> level;
 	harl.complain(level);
 }
