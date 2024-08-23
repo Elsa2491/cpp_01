6bea14e45dc7ca84bc31334ba1b24d98880ba6cb
elsa2491 <elsa.touma@gmail.com>
2024-08-23 14:41:07 +0200

    Clean

--- ex03/main.cpp
+++ ex03/main.cpp
@@ -6,37 +6,13 @@
 /*   By: eltouma <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/16 13:31:09 by eltouma           #+#    #+#             */
-/*   Updated: 2024/08/16 17:58:40 by eltouma          ###   ########.fr       */
+/*   Updated: 2024/08/23 14:40:34 by eltouma          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Weapon.hpp"
 #include "HumanA.hpp"
 #include "HumanB.hpp"
-
-/*
-int	main(void)
-{
-	{
-		Weapon club = Weapon("crude skiped club");
-
-		HumanA bob("Bob", club);
-		bob.attack();
-		club.setType("some other type of club");
-		bob.attack();
-	}
-	{
-		Weapon club = Weapon("crude spiked club");
-
-		HumanB jim("Jim");
-		jim.setWeapon(club);
-		jim.attack();
-		club.setType("some other type of club");
-		jim.attack();
-	}
-	return 0;
-}
-*/
 
 int main()
 {
@@ -50,7 +26,7 @@ int main()
     {
         Weapon club = Weapon("crude spiked club");
         HumanB jim("Jim");
-        jim.attack();
+	// jim.attack();
         jim.setWeapon(club);
         jim.attack();
         club.setType("some other type of club");
