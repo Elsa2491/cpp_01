2351eaf23005d542209a28f76c10716a8991f5ab
elsa2491 <elsa.touma@gmail.com>
2024-08-23 16:23:25 +0200

    [23.08 Clean] Clean main()

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: eltouma <eltouma@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/21 12:42:33 by eltouma           #+#    #+#             */
-/*   Updated: 2024/08/23 16:14:18 by eltouma          ###   ########.fr       */
+/*   Updated: 2024/08/23 16:21:55 by eltouma          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,13 @@ int main(int argc, char **argv)
 {
 	std::string outfile_name; 
 
-	if (argc != 4)
+	if (argc != 4 || !argv[2][0])
 	{
-		std::cout << "Failre:\nWrong number of arguments" << std::endl;
-		return (1);
-	}
-	if (!argv[2][0])
-	{
+		if (argc != 4)
+		{
+			std::cout << "Failre:\nWrong number of arguments" << std::endl;
+			return (1);
+		}
 		std::cout << "Failure:\nString to replace can't be empty" << std::endl;
 		return (1);
 	}
